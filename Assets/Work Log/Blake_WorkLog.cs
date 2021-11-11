ed3f242edbd08c7f1812ef8caf32bed661f7817a
Ghost2600 <90476678+Ghost2600@users.noreply.github.com>
2021-11-11 17:21:51 -0500

    Added the ladder & tried to make a barrel

--- Assets/Work Log/Blake_WorkLog.cs
+++ Assets/Work Log/Blake_WorkLog.cs
@@ -3,6 +3,8 @@ class Blake_WorkLog
     /*
     Blake's Work
     Made Scaffolding Prefab (1 hour)
+    Added the ladder prefab (15 minutes)
+    Fixed a few things (20 minutes)
 
     Total Time: 
     */
