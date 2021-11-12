43caab1a3b0e07bba7aea6fbb73a27aa363c3659
Ghost2600 <90476678+Ghost2600@users.noreply.github.com>
2021-11-11 19:51:11 -0500

    Added test stuff

--- Assets/Work Log/Blake_WorkLog.cs
+++ Assets/Work Log/Blake_WorkLog.cs
@@ -4,7 +4,9 @@ class Blake_WorkLog
     Blake's Work
     Made Scaffolding Prefab (1 hour)
     Added the ladder prefab (15 minutes)
-    Fixed a few things (20 minutes)
+    Fixed a few things (15 minutes)
+    Added a few undeeded things (5 minutes)
+    Added Scaffolding test area for player (5 minutes)
 
     Total Time: 
     */
