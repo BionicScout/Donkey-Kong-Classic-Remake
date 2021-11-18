da5707332f68591c75b6f6f1476c148b671ca053
Bionic_Scout <roman.higginson@zoominternet.net>
2021-11-18 09:23:23 -0500

    Uhhh

--- Assets/Work Log/Blake_WorkLog.cs
+++ Assets/Work Log/Blake_WorkLog.cs
@@ -7,6 +7,7 @@ class Blake_WorkLog
     Fixed a few things (15 minutes)
     Added a few undeeded things (5 minutes)
     Added Scaffolding test area for player (5 minutes)
+    Just added and rescaled the cursed DK (1 minute)
 
     Total Time: 
     */
