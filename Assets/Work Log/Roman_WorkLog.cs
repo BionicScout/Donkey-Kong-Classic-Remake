6c7d291eed8b896f239771bf8f8c7ea2bd5f7bdc
Bionic_Scout <roman.higginson@zoominternet.net>
2021-11-18 18:39:40 -0500

    Animation Finished

--- Assets/Work Log/Roman_WorkLog.cs
+++ Assets/Work Log/Roman_WorkLog.cs
@@ -3,7 +3,9 @@ class Roman_WorkLog
     /*
     Roman's Work
     Setting up Respoitory - 1.5 hour
+    Making Donkey Kong Preefab - 1 hour
+    Donkey Kong Walking Animation - 3 hours
 
-    Total Time: 1.5 hours
+    Total Time: 5.5 hours
     */
 }
