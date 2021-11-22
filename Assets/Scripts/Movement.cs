dbc91c670c449d09511eba8880ca6745f8a2dfce
Bionic_Scout <roman.higginson@zoominternet.net>
2021-11-22 10:37:23 -0500

    Updated Movement and Added Level 1

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -6,57 +6,56 @@ public class Movement : MonoBehaviour
 {
     public Rigidbody DKbody;
     public float speed = 3.5f;
-    public float jump = 3f;
+    public float jumpHeight = 3f;
     public float rotateSpeed = 3f;
 
     bool direction = false; //true = left, false = right
+    bool jump = false;
 
 
 
     void Update() {
-        //Update button press
-        float currentSpeed = 0; ;
+        //Update sideways motion
+        float currentSpeed = 0;
 
         if (Input.GetKey(KeyCode.D)) {
             currentSpeed = speed;
-            if (direction)
-            {
+
+            if (direction)            
                 direction = false;
-            }
         }
         else if (Input.GetKey(KeyCode.A))
         {
             currentSpeed = -1 * speed;
 
-            if (!direction)
-            {
+            if (!direction)            
                 direction = true;
-            }
         }
 
         DKbody.velocity = new Vector3(0, DKbody.velocity.y, currentSpeed);
 
 
-
+    //Rotate DK on direction switch
         int dir = 1;
 
-        if (direction)
-        {
+        if (direction)       
             dir = 0;
-        }
-
-        // Rotate the cube by converting the angles into a quaternion.
-        Quaternion change = Quaternion.Euler(0, 180 * dir, 0);
-
-        // Dampen towards the target rotation
-        transform.rotation = Quaternion.Slerp(transform.rotation, change, Time.deltaTime * rotateSpeed);
+        
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, 180 * dir, 0), Time.deltaTime * rotateSpeed);
 
         //Update Jump
-        if (Input.GetKeyDown(KeyCode.W) && DKbody.velocity.y == 0)
+        if (Input.GetKeyDown(KeyCode.W) && !jump)
         {
-            DKbody.AddForce(0, jump, 0, ForceMode.Acceleration);
+            DKbody.AddForce(0, jumpHeight, 0, ForceMode.Acceleration);
+            jump = true;
         }
 
-        Debug.Log(DKbody.velocity.y);
+        Debug.Log(DKbody.velocity);
+    }
+
+    private void OnCollisionEnter(Collision collision){
+        if(collision.gameObject.tag == "Floor"){
+            jump = false;
+        }
     }
 }
