af6312ac7ee5dbfe6a95bcd06dc7781c9795ca68
Ghost2600 <90476678+Ghost2600@users.noreply.github.com>
2021-11-24 17:58:29 -0500

    Added animation script
    Still needs work

--- Assets/Work Log/Blake_WorkLog.cs
+++ Assets/Work Log/Blake_WorkLog.cs
@@ -5,11 +5,12 @@ class Blake_WorkLog
     Made Scaffolding Prefab (1 hour)
     Added the ladder prefab (15 minutes)
     Fixed a few things (15 minutes)
-    Added a few undeeded things (5 minutes)
     Added Scaffolding test area for player (5 minutes)
     Just added and rescaled the cursed DK (1 minute)
     Quick Broken Bridge animation (2 minutes)
+    Created Spear, Mario and Peach modles (1 hour 40 minutes)
+    Created Barrel modles and effects (30 minutes)
 
-    Total Time: 
-    */
+    Total Time: 3 hours 8 minutes
+    */ 
 }
